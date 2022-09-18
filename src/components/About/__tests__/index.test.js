b6921c3bba4a6bc2df22f85c45e8b07571a91d34
unknown <pratttrevor79@gmail.com>
2022-09-18 08:02:44 -0500

    broken tests, moving on

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -3,8 +3,7 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
 
-const { asFragment } = render(<About/>);
-expect(asFragment()).toMatchSnapshot();
+
 
 afterEach(cleanup);
 
@@ -13,10 +12,13 @@ describe('About component', () => {
 
     //First Test
     it('renders', () => {
-        render(<About/>);
+        render(<About />);
     })
     //Second Test
     it('matches snapshop DOM node structure', () => {
         // render About
-    })
+        const { asFragment } = render(<About />);
+
+        expect(asFragment()).toMatchSnapshot();
+    });
 })
